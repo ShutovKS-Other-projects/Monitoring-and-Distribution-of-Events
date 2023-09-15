6563d2264f66eb332a0d5d0afe4e57ec69c4c1fb
ShutovKS <i@shutovks.ru>
2023-09-15 13:06:02 +0300

    Added get links on hackatons from "хакатоны.рус"

---
+++ src/Backend/SourceMonitoring/app/api/hackathons_rus/main_page.py
@@ -0,0 +1,17 @@
+from app.tools.browser import get_response
+import json
+
+__url = "https://feeds.tildacdn.com/api/getfeed/?feeduid=617755803461&recid=488758159&c=1694767429185&size=16&slice=1&sort%5Bdate%5D=desc&filters%5Bdate%5D%5B%3C%5D=now&getparts=true"
+
+
+def get_links_to_hackathons():
+    response = get_response(__url)
+    urls = []
+
+    result = response.text
+    json_text = json.loads(result)
+    posts = json_text['posts']
+    for post in posts:
+        urls.append(post['url'])
+
+    return urls
